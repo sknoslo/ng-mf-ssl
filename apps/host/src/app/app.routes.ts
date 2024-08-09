4519b79ec45661a80e1ff44072d9fddcbc80bd12
Steffen Olson <solson@relias.com>
2024-08-09 08:56:06 -0400

    g todo remote

--- apps/host/src/app/app.routes.ts
+++ apps/host/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
+  {
+    path: 'todo',
+    loadChildren: () =>
+      loadRemoteModule('todo', './Routes').then((m) => m.remoteRoutes),
+  },
   {
     path: '',
     component: NxWelcomeComponent,
